e546f596a604448ab3a9eda5fd35845cf29f202f
maanika <kennethmaanika@gmail.com>
2021-02-18 09:41:02 +1100

    Move component files inside a folder

---
+++ Git_Add_Commit_Push.ps1
@@ -0,0 +1,24 @@
+# Refer to the instructions for using PSoC Creator with version control:
+# https://community.cypress.com/docs/DOC-11030
+
+# Add the PSOC Creator Files
+git add *.c *.h *.cydwr *.cyprj .\TopDesign\*.cysch
+
+# Add additional files
+git add *.md
+
+# Added these files
+echo 'Git Status:'
+git status
+
+# Ask for the commit message
+$CommitMsg = Read-Host -Prompt 'Enter the commit message'
+
+# Commit
+git commit --m "$CommitMsg"
+
+# Push
+git push -u origin main
+
+# Keep the console open after execution
+Read-Host -Prompt 'Press Enter to exit'
