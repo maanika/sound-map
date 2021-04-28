209da680cb3dea0532fdf4e9639d198b61e1eaf1
maanika <kennethmaanika@gmail.com>
2021-04-29 09:56:39 +1000

    update compass files

--- components/lsm303d.c
+++ components/lsm303d.c
@@ -1,13 +1,13 @@
 /*******************************************************************************
 * Written by : Maanika Kenneth Koththigoda (kennethmaanika@gmail.com)
-* Last Modified : 25/10/2020
+* Last Modified : 13/03/2021
 *
 * @file    LSM303D.c
 * @version 1.0.0
 *
 * @brief Library for PSoC compatible devices that interfaces with LSM303D compass 
-*        and accelerometer ICs on Pololu boards. It allows to read the raw 
-*        accelerometer and magnetometer data from LSM303D.
+*        and accelerometer ICs on Adafruit boards. It allows to read the raw 
+*        accelerometer and magnetometer data from LSM303DHLC.
 *
 * @par Target device
 *    CY8C5888LTI - LP097
@@ -23,51 +23,36 @@
 *    - RST_1       [I2C_1]
 *    - Clock_I2C_1 [I2C_1]
 *
-* @par Source
-*    LSM303D library code for PSoC 5LP is adapted from 
-*    https://github.com/pololu/lsm303-arduino
-*
 *    I2C Read and Write functions using low level API 
 *    is taken from https://community.cypress.com/docs/DOC-15336
-
-* @note
-*   Calibration values are by default m_min_x = -7733; m_min_y =  -4797;
-*   m_min_z = -2535; m_max_x = -1979; m_max_y = +875; m_max_z = +642;
-*   Use the Calibrate example program from Arduino to determine 
-*   appropriate values for your particular unit.
 *
 *******************************************************************************/
 #include "project.h"
 #include "lsm303d.h"
-
 #include "math.h"
 #include "stdio.h"
 
 /*******************************************************************************
 *   Constant definitions
 *******************************************************************************/
-// @brief Pi Constant
+// Pi Constant
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 
-// @brief Sub adress length
+// Sub adress length
 #define SUB_ADDRESS_LENGTH (0x01u)
 
-//  @brief Length of data to be read.
+//  Length of data to be read.
 #define READ_DATA_LENGTH (1u)
 
-// @brief Length of data to be read, includes register and data
+// Length of data to be read, includes register and data
 #define WRITE_DATA_LENGTH (2u)
 
-// @brief Axis values
+// Axis values
 #define XAXIS 0x00
 #define YAXIS 0x01
 #define ZAXIS 0x02
-
-// @brief LSM303D Regsiters
-
-
 
 /*******************************************************************************
 * Function Name: I2C_Read
@@ -165,36 +150,28 @@ static void I2C_Write(uint8 slaveAddress, uint8 registerAddress, uint8 wrData) {
 
 
 /*******************************************************************************
-* Function Name: compassStart
+* Function Name: Compass_Initialize
 ********************************************************************************
-* Summary
+* @par Summary
+*   Enables the LSM303DHLC's accelerometer and magnetometer. 
+*   Sets the sensor's full scales (gain) to default power-on values, 
+*   which are +/- xxx g for accelerometer and +/- xxxx gauss.
+*   Selects xxx Hz ODR (output data rate) for accelerometer and xxx Hz
+*   ODR for magnetometer (These are the ODR settings for which the 
+*   electrical characteristics are specified in the datasheets).
+*   Enables high resolution modes (if available).
+*
 *******************************************************************************/
 void compassStart() {
     
     I2C_1_Start();
     
-}
-/*******************************************************************************
-* Function Name: compassInitialize
-********************************************************************************
-* @par Summary
-*   Enables the LSM303's accelerometer and magnetometer. 
-*   Sets the sensor's full scales (gain) to default power-on values, 
-*   which are +/- 2 g for accelerometer and +/- 4 gauss.
-*   Selects 50 Hz ODR (output data rate) for accelerometer and 6.25 Hz
-*   ODR for magnetometer (These are the ODR settings for which the 
-*   electrical characteristics are specified in the datasheets).
-*   Enables high resolution modes (if available).
-*   Note that this function will also reset other settings controlled by
-*   the registers it writes to.
-*******************************************************************************/
-void compassInitialize() {
+    I2C_Write(0x19, 0x20, 0x27u);//set CTRL_REG1_A register
+    I2C_Write(0x19, 0x23, 0x08u);//set CTRL_REG4_A register
     
-    //I2C_Write(0x18, 0x20, 0x27);//set CTRL_REG1_A register
-    //I2C_Write(0x18, 0x23, 0x40);//set CTRL_REG4_A register
-    I2C_Write(0x1E, 0x00, 0x14);//set CRA_REG_M register
-    I2C_Write(0x1E, 0x02, 0x00);//set MR_REG_M register
-    
+    I2C_Write(0x1E, 0x00, 0x0Cu);//set CRA_REG_M register
+    I2C_Write(0x1E, 0x01, 0x20u); // SET CRB_REG_M 
+    I2C_Write(0x1E, 0x02, 0x00u);//set MR_REG_M register
 }
 
 /*******************************************************************************
@@ -208,22 +185,21 @@ static short accelerometer(int axis){
     unsigned char ACC_Data[2];
     short Raw;
     
-    if (axis==0) {
-        ACC_Data[0] = I2C_Read(0x18, 0x28);//read OUT_X_L_A (MSB)
-        ACC_Data[1] = I2C_Read(0x18, 0x29);//read OUT_X_H_A (LSB)
+    if (axis == 0) {
+        ACC_Data[0] = I2C_Read(0x19, 0x28);//read OUT_X_L_A (MSB)
+        ACC_Data[1] = I2C_Read(0x19, 0x29);//read OUT_X_H_A (LSB)
     }
-    else if (axis==1) {
-        ACC_Data[0] = I2C_Read(0x18, 0x2A);//read OUT_Y_L_A (MSB)
-        ACC_Data[1] = I2C_Read(0x18, 0x2B);//read OUT_Y_H_A (LSB)
+    else if (axis == 1) {
+        ACC_Data[0] = I2C_Read(0x19, 0x2A);//read OUT_Y_L_A (MSB)
+        ACC_Data[1] = I2C_Read(0x19, 0x2B);//read OUT_Y_H_A (LSB)
     }
-    else if (axis==2) {
-        ACC_Data[0] = I2C_Read(0x18, 0x2C);//read OUT_Z_L_A (MSB)
-        ACC_Data[1] = I2C_Read(0x18, 0x2D);//read OUT_Z_H_A (LSB)
+    else if (axis == 2) {
+        ACC_Data[0] = I2C_Read(0x19, 0x2C);//read OUT_Z_L_A (MSB)
+        ACC_Data[1] = I2C_Read(0x19, 0x2D);//read OUT_Z_H_A (LSB)
     }
     
     Raw = (short) (( ((unsigned short) ACC_Data[0]) << 8) + ((unsigned short) ACC_Data[1]));
     return Raw;
-
 }
 
 /*******************************************************************************
@@ -237,57 +213,38 @@ static short magnetometer(int axis){
     unsigned char MR_Data[2];
     short Raw;
     
-    if (axis==0) {
-        MR_Data[0] = I2C_Read(0x1E, 0x03 );//read OUT_X_H_M (MSB)
-        MR_Data[1] = I2C_Read(0x1E, 0x04);//read OUT_X_L_M (LSB)
+    if (axis == 0) {
+        MR_Data[0] = I2C_Read( 0x1E, 0x03 );//read OUT_X_H_M (MSB)
+        MR_Data[1] = I2C_Read( 0x1E, 0x04 );//read OUT_X_L_M (LSB)
     }
-    else if (axis==1) {
-        MR_Data[0] = I2C_Read(0x1E, 0x07);//read OUT_Y_H_M (MSB)
-        MR_Data[1] = I2C_Read(0x1E, 0x08);//read OUT_Y_L_M (LSB)
+    else if (axis == 1) {
+        MR_Data[0] = I2C_Read( 0x1E, 0x07 );//read OUT_Y_H_M (MSB)
+        MR_Data[1] = I2C_Read( 0x1E, 0x08 );//read OUT_Y_L_M (LSB)
     }
-    else if (axis==2) {
-        MR_Data[0] = I2C_Read(0x1E, 0x05);//read OUT_Z_H_M (MSB)
-        MR_Data[1] = I2C_Read(0x1E, 0x06);//read OUT_Z_L_M (LSB)
+    else if (axis == 2) {
+        MR_Data[0] = I2C_Read( 0x1E, 0x05 );//read OUT_Z_H_M (MSB)
+        MR_Data[1] = I2C_Read( 0x1E, 0x06 );//read OUT_Z_L_M (LSB)
     }
     
     Raw = (short) (( ((unsigned short) MR_Data[0]) << 8) + ((unsigned short) MR_Data[1]));
     return Raw;
+}
 
-}
 /*******************************************************************************
-* Function Name: readMag
+* Function Name: compassRead
 ********************************************************************************
 * @par Summary
-*   Reads the magnetic data from all axes.
+*   Reads both the magnetic and acceleration data from all axes.
 *******************************************************************************/
-static void readMag(double *return_m_x, double *return_m_y, double *return_m_z)
-{
-    double m_x, m_y, m_z;
-    
-    *return_m_x = magnetometer(XAXIS);
-    *return_m_y = magnetometer(YAXIS);
-    *return_m_z = magnetometer(ZAXIS);
-    
-}
-/*******************************************************************************
-* Function Name: heading
-********************************************************************************
-* @par Summary
-*   Returns the angular difference in the horizontal plane between a
-*   default vector and north, in degrees.
-*   The default vector here is chosen to point along the surface of the
-*   PCB, in the direction of the top of the text on the silkscreen.
-*   This is the +X axis on the Pololu LSM303D carrier.
-*******************************************************************************/
-double heading()
-{
-    double m_x, m_y, m_z;
-    readMag(&m_x, &m_y, &m_z);
-    
-    double heading = (atan2(-m_y, m_x)); //* (180 / M_PI);
-    
-    if (heading < 0) heading = heading + (2*M_PI);
-    return heading;
+void compassRead(compassRaw *compassData)
+{   
+    compassData->m_x = magnetometer(XAXIS);
+    compassData->m_y = magnetometer(YAXIS);
+    compassData->m_z = magnetometer(ZAXIS);
+   
+    compassData->a_x = accelerometer(XAXIS);
+    compassData->a_y = accelerometer(YAXIS);
+    compassData->a_z = accelerometer(ZAXIS);   
 }
 
 /* [] END OF FILE */
--- components/lsm303d.h
+++ components/lsm303d.h
@@ -1,13 +1,13 @@
 /*******************************************************************************
 * Written by : Maanika Kenneth Koththigoda (kennethmaanika@gmail.com)
-* Last Modified : 25/10/2020
+* Last Modified : 13/03/2021
 *
-* @file    lsm303d.h
+* @file    LSM303D.c
 * @version 1.0.0
 *
-* @brief Library for PSoC devices that interfaces with LSM303D compass
-*        and accelerometer ICs on Pololu boards. It allows to read the raw 
-*        accelerometer and magnetometer data from LSM303D
+* @brief Library for PSoC compatible devices that interfaces with LSM303D compass 
+*        and accelerometer ICs on Adafruit boards. It allows to read the raw 
+*        accelerometer and magnetometer data from LSM303DHLC.
 *
 * @par Target device
 *    CY8C5888LTI - LP097
@@ -23,34 +23,30 @@
 *    - RST_1       [I2C_1]
 *    - Clock_I2C_1 [I2C_1]
 *
-* @par Source
-*    LSM303D library code for PSoC 5LP is adapted from https://github.com/pololu/lsm303-arduino
-*
 *    I2C Read and Write functions using low level API 
 *    is taken from https://community.cypress.com/docs/DOC-15336
-
-* @note
-*   Calibration values are by default min{-7733, -4797, -2535} 
-*   and max{ -1979, +875, +642}
-*   Use the Calibrate example program from Arduino to determine 
-*   appropriate values for your particular unit
 *
 *******************************************************************************/
 #include "project.h"
 
+typedef struct compassRaw{
+    double m_x;
+    double m_y;
+    double m_z;
+    double a_x;
+    double a_y;
+    double a_z;
+} compassRaw;
+
 /*******************************************************************************
 *   Function Declarations
 *******************************************************************************/
-
-void compassStart();
-
-
 // @brief Initializes all accelerometer and magnetometer, Sets values to defaults
 // @return '1' if sucess or '0' if fail
-void compassInitialize();
-
-// @brief Calculates the magnetic heading with respect to the y axis
-// @return heading value
-double heading(void);
+void compassStart();
+    
+// @brief Reads all magnetometer and accelerometer data registers
+// @param return varaibles
+void compassRead(compassRaw *compassData);
 
 /* [] END OF FILE */
